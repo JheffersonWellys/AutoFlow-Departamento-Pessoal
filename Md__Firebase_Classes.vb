c1d2b0f5659e1fffe889d4bce66bab3c29e3de63
jhefferson <jhefferson.w.dev@gmail.com>
2025-05-17 10:24:07 -0300

    feat(firebase): adicionar integracao com firebase
    - Adicionar modulo Md__Firebase_Classes.vb com configuracoes do firebase
    - Adicionar modulo Md__VariaveisGlobais.vb para variaveis globais
    - Adicionar pacotes FirebaseDatabase.net, LiteDB e Newtonsoft.Json
    - Atualizar packages.config com as dependencias instaladas
    - Configurar projeto para suporte a conexao com firebase

---
+++ Md__Firebase_Classes.vb
@@ -0,0 +1,53 @@
+﻿Imports Firebase.Database
+
+Module Md__Firebase_Classes
+
+    Public Class Usuario
+
+        Public Property Id As Integer
+        Public Property Chapa As String
+        Public Property Email As String
+        Public Property NomeCompleto As String
+        Public Property Token As String
+
+    End Class
+
+    Public Class FirebaseHelper
+
+        Private Shared _firebaseClient As FirebaseClient
+
+        Public Shared ReadOnly Property FirebaseClient As FirebaseClient
+
+            Get
+                If _firebaseClient Is Nothing Then
+
+                    _firebaseClient = New FirebaseClient(Firebase__DatabaseURL)
+
+                End If
+
+                Return _firebaseClient
+
+            End Get
+
+        End Property
+
+    End Class
+
+    Public Class FirebaseAuthResponse
+
+        Public Property idToken As String
+        Public Property email As String
+        Public Property refreshToken As String
+        Public Property expiresIn As String
+        Public Property localId As String
+
+    End Class
+
+    Public Class AuthResult
+
+        Public Property IdToken As String
+        Public Property LocalId As String
+
+    End Class
+
+End Module
---
+++ Md__VariaveisGlobais.vb
@@ -0,0 +1,13 @@
+﻿Module Md__VariaveisGlobais
+
+#Region "VARIÁVEIS FIREBASE"
+
+    Public ReadOnly Firebase__BaseURL As String = "https://identitytoolkit.googleapis.com/v1/accounts:"
+    Public ReadOnly Firebase__DatabaseURL As String = "https://facilitadp-default-rtdb.firebaseio.com/"
+
+    Public TokenUsuarioLogado As String = ""
+    Public TokenUsuario As String = ""
+
+#End Region
+
+End Module

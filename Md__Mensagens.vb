9e406a3307d77202add1594eb75183bbc0c7fcee
jhefferson <jhefferson.w.dev@gmail.com>
2025-05-17 11:14:05 -0300

    feat(firebase): adicionar gerenciamento de mensagens e api key
    - Adicionar modulo Md__Mensagens centralizando exibicao padronizada
    - Adicionar Md__Firebase_Funcoes com funcoes de validacao de api key
    - Adicionar chamada para validar api key no carregamento do Frm__Intro
    - Alterar referencias e dependencias no projeto DP para incluir novos modulos

---
+++ Md__Mensagens.vb
@@ -0,0 +1,77 @@
+﻿Module Md__Mensagens
+
+#Region "ENUMERADORES"
+
+    Public Enum TipoMensagem
+        Erro
+        Alerta
+        Sucesso
+        Confirmacao
+    End Enum
+
+#End Region
+
+#Region "FUNÇÕES DE EXIBIÇÃO DE MENSAGENS"
+
+    Private Sub ExibirMensagem(Mensagem As String, Tipo As TipoMensagem)
+
+        Dim titulo As String
+        Dim icone As MessageBoxIcon
+        Dim botoes As MessageBoxButtons = MessageBoxButtons.OK
+
+        Select Case Tipo
+
+            Case TipoMensagem.Erro
+                titulo = "AutoFlow - Erro!"
+                icone = MessageBoxIcon.Error
+
+            Case TipoMensagem.Alerta
+                titulo = "AutoFlow - Aviso!"
+                icone = MessageBoxIcon.Warning
+
+            Case TipoMensagem.Sucesso
+                titulo = "AutoFlow - Sucesso!"
+                icone = MessageBoxIcon.Information
+
+            Case TipoMensagem.Confirmacao
+                titulo = "AutoFlow - Confirmação!"
+                icone = MessageBoxIcon.Question
+                botoes = MessageBoxButtons.YesNo
+
+            Case Else
+                titulo = "AutoFlow"
+                icone = MessageBoxIcon.Information
+
+        End Select
+
+        MessageBox.Show(Mensagem, titulo, botoes, icone)
+
+    End Sub
+
+    Public Sub ExibirMensagem__Erro(Mensagem As String)
+
+        ExibirMensagem(Mensagem, TipoMensagem.Erro)
+
+    End Sub
+
+    Public Sub ExibirMensagem__Alerta(Mensagem As String)
+
+        ExibirMensagem(Mensagem, TipoMensagem.Alerta)
+
+    End Sub
+
+    Public Sub ExibirMensagem__Sucesso(Mensagem As String)
+
+        ExibirMensagem(Mensagem, TipoMensagem.Sucesso)
+
+    End Sub
+
+    Public Function ExibirMensagem__Confirmacao(Mensagem As String) As DialogResult
+
+        Return MessageBox.Show(Mensagem, "AutoFlow - Confirmação!", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+    End Function
+
+#End Region
+
+End Module
--- Md__VariaveisGlobais.vb
+++ Md__VariaveisGlobais.vb
@@ -1,12 +1,21 @@
-﻿Module Md__VariaveisGlobais
+﻿Imports System.IO
+
+Module Md__VariaveisGlobais
 
 #Region "VARIÁVEIS FIREBASE"
 
     Public ReadOnly Firebase__BaseURL As String = "https://identitytoolkit.googleapis.com/v1/accounts:"
-    Public ReadOnly Firebase__DatabaseURL As String = "https://facilitadp-default-rtdb.firebaseio.com/"
+    Public ReadOnly Firebase__DatabaseURL As String = "https://autoflow-departamentopessoal-default-rtdb.firebaseio.com/"
 
     Public TokenUsuarioLogado As String = ""
     Public TokenUsuario As String = ""
+
+    Public CaminhoToken As String = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+        "AutoFlow",
+        My.Settings.AutoFlow__ModuloSistema,
+        "Token.tmp"
+    )
 
 #End Region
 
